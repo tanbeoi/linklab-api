f30e04c984db681266630e4c6cdc1a3e8cf2dbac
Duy Tan Pham <tanphamduy24082005@gmail.com>
2026-09-12 08:10:47 +1000

    feat(api): allow requests from local frontend

--- LinkLab.Api/Program.cs
+++ LinkLab.Api/Program.cs
@@ -119,7 +119,21 @@ builder.Services
 
 builder.Services.AddAuthorization();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Frontend", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("Frontend");
+
 
 app.UseExceptionHandler();
 
